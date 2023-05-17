e5f33d0bdc85a7b18225eb9a2c886a1529b3331d
Sasha Raimov <crazyysasha@gmail.com>
2023-05-17 09:14:57 +0500

    Delete Responsive Modals

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+  content: ["index.html", "bavoo.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
